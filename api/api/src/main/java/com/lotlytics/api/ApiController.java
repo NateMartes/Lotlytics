aa7efdb6cabdcd2902cab0e63c6e5e58dd125e21
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-09-24 15:36:17 -0400

    feat: Added init API Springboot application

---
+++ api/api/src/main/java/com/lotlytics/api/ApiController.java
@@ -0,0 +1,7 @@
+package com.lotlytics.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApiController {
+}
---
+++ api/api/src/test/java/com/lotlytics/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotlytics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
