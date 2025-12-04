83e0ed61bb2edb6d91f45558b5c90fcc4290ff32
Nathaniel Martes <nathaniel.martes.business@gmail.com>
2025-12-03 23:52:54 -0500

    fix: Fix lot create endpoint

--- client/app/admin/dashboard/create-lot/page.tsx
+++ client/app/admin/dashboard/create-lot/page.tsx
@@ -122,7 +122,7 @@ export function CreateLotForm() {
         };
 
         try {
-            const response = await fetch(`https://lotlytics-api.nathanielmartes.com/api/v1/?groupId=${groupId}`, {
+            const response = await fetch(`https://lotlytics-api.nathanielmartes.com/api/v1/lot?groupId=${groupId}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
